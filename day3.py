e320f319417635719e736d4ef57109f8d01260ab
Christoph Terasa <christoph@kohlio.de>
2015-12-13 20:47:16 +0100

    day3.py: Added solution for part1 of day 3.

---
+++ day3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import fileinput
+
+def move(c):
+    if c is "^":
+        return 0+1j
+    elif c is ">":
+        return 1+0j
+    elif c is "<":
+        return -1+0j
+    elif c is "v":
+        return 0-1j
+
+def houses(s):
+    h = [0 + 0j]
+    u = [0 + 0j]
+    for ss in s:
+        new = h[-1] + move(ss)
+        h += [new]
+        if new not in u:
+            u += [new]
+    return h, u
+
+def part1(s):
+    h, u = houses(s)
+    return len(u)
+
+if __name__ == '__main__':
+    s = [line.strip() for line in fileinput.input()][0]
+    print "Part 1: ", part1(s)
+    #print "Part 2: ", part2(s)
