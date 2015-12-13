f1039a2cf85dc06c6eadab23da95b6f04eecdcb9
Christoph Terasa <christoph@kohlio.de>
2015-12-13 19:59:02 +0100

    day1.py: Added solutions for part1 and part2 of day 1.

---
+++ day1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import fileinput
+
+def up_or_down(c):
+    if c is '(':
+        return 1
+    elif c is ')': 
+        return -1
+    else:
+        return 0
+
+def part1(s):
+    fl = [up_or_down(c) for c in s]
+    return sum(fl)
+
+def part2(s):
+    fl = 0
+    for i, c in enumerate(s):
+        if fl == -1:
+            return i
+        fl += up_or_down(c)
+
+if __name__ == '__main__':
+    s = [line for line in fileinput.input()][0]
+    print "Part 1: ", part1(s)
+    print "Part 2: ", part2(s)
