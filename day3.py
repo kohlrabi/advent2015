e0342cae78252ef837501fb999a44bf12f664839
Christoph Terasa <christoph@kohlio.de>
2015-12-13 20:51:46 +0100

    day3.py: Added solution for part2 of day 3.

--- day3.py
+++ day3.py
@@ -26,7 +26,15 @@ def part1(s):
     h, u = houses(s)
     return len(u)
 
+def part2(s):
+    h, u = houses(s[0::2])
+    h2, u2 = houses(s[1::2])
+    for uu in u2:
+        if not uu in u:
+            u += [uu]
+    return len(u)
+
 if __name__ == '__main__':
     s = [line.strip() for line in fileinput.input()][0]
     print "Part 1: ", part1(s)
-    #print "Part 2: ", part2(s)
+    print "Part 2: ", part2(s)
