e3270d046e1bc4955e647b83b214ec905b3ee63a
Christoph Terasa <christoph@kohlio.de>
2015-12-14 06:23:50 +0100

    day4.py: Added solution for part2 of day 4.

--- day4.py
+++ day4.py
@@ -7,13 +7,19 @@ import math
 def salt(s, n):
     return s + str(n)
     
-def part1(s):
-    for i in xrange(10000000000):
+def coin(s, n=5):
+    for i in xrange(10000000000000):
         sa = md5.md5(salt(s, i)).hexdigest()
-        if sa[0:5] == '00000':
+        if sa[0:n] == '0'*n:
             return i
+
+def part1(s):
+    return coin(s, 5)
+
+def part2(s):
+    return coin(s, 6)
 
 if __name__ == '__main__':
     s = [line.strip() for line in fileinput.input()][0]
     print "Part 1: ", part1(s)
-    #print "Part 2: ", part2(s)
+    print "Part 2: ", part2(s)
