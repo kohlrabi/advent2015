453c9e213c6961269402387581c2fc19c9a91524
Jan Christoph Terasa <christoph@kohlio.de>
2020-02-27 15:04:56 +0100

    Implement part 2 of day 05.

--- python/day05.py
+++ python/day05.py
@@ -12,17 +12,32 @@ def nice(s):
     re_vowels = re.findall(r'(.*[aeiou].*){3,}', s)
     if not re_vowels:
         return False
-    re_double = re.findall(r'(.)\1{1,}', s)
+    re_double = re.findall(r'(.)\1', s)
     if re_double:
         return True
     return False
 
 
+def nice2(s):
+    re_twice = re.findall(r'(..).*\1', s)
+    re_repeats = re.findall(r'(.).\1', s)
+
+    if re_twice and re_repeats:
+        return True
+    return False
+
+
 def part1(s):
-    ni = [l for l in s if nice(l)]
+    ni = [True for l in s if nice(l)]
+    return len(ni)
+
+
+def part2(s):
+    ni = [True for l in s if nice2(l)]
     return len(ni)
 
 
 if __name__ == '__main__':
     s = [line.strip() for line in fileinput.input()]
     print("Part 1:", part1(s))
+    print("Part 2:", part2(s))
