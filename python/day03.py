93faa76b2a962763db00f0bd095bf798a634e6bd
Jan Christoph Terasa <christoph@kohlio.de>
2020-02-27 13:54:05 +0100

    Simplify day03.py.

--- python/day03.py
+++ python/day03.py
@@ -3,23 +3,16 @@
 import fileinput
 
 def move(c):
-    if c is "^":
-        return 0+1j
-    elif c is ">":
-        return 1+0j
-    elif c is "<":
-        return -1+0j
-    elif c is "v":
-        return 0-1j
+    d = {'^': 0+1j, '>': 1+0j, '<': -1+0j, 'v': 0-1j}
+    return d[c]
 
 def houses(s):
     h = [0 + 0j]
-    u = [0 + 0j]
+    u = {0 + 0j}
     for ss in s:
         new = h[-1] + move(ss)
-        h += [new]
-        if new not in u:
-            u += [new]
+        h.append(new)
+        u.add(new)
     return h, u
 
 def part1(s):
@@ -31,7 +24,7 @@ def part2(s):
     h2, u2 = houses(s[1::2])
     for uu in u2:
         if not uu in u:
-            u += [uu]
+            u.add(uu)
     return len(u)
 
 if __name__ == '__main__':
