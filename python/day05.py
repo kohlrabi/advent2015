25f402f603bba65fb5d4dad171dbd59fc6fb5cc6
Jan Christoph Terasa <christoph@kohlio.de>
2020-02-27 14:40:41 +0100

    Use regexen for day05, part1.

--- python/day05.py
+++ python/day05.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
 
 import fileinput
+import re
+
 
 def nice(s):
-    vowels = 'aeiou'
-    bad = {'ab', 'cd', 'pq', 'xy'}
 
-    for b in bad:
-        if b in s:
-            return False
+    re_bad = re.compile(r'ab|cd|pq|xy')
+    re_vowels = re.compile(r'[aeiou]')
+    re_double = re.compile(r'(.)\1{1,}')
 
-    vows = 0
-    for v in vowels:
-        vows += s.count(v)
-    if vows < 3:
+    if re_bad.findall(s):
         return False
+    nvowels = len(re_vowels.findall(s))
+    if nvowels < 3:
+        return False
+    if re_double.findall(s):
+        return True
 
-    for i in range(97,123,1):
-        if chr(i)*2 in s:
-            return True
-            
     return False
+
 
 def part1(s):
     ni = [l for l in s if nice(l)]
     return len(ni)
 
+
 if __name__ == '__main__':
     s = [line.strip() for line in fileinput.input()]
     print("Part 1:", part1(s))
-    #print "Part 2: ", part2(s)
+    # print "Part 2: ", part2(s)
+    # print "Part 2: ", part2(s)
